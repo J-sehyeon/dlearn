a6d2b549324acf339abcb573864029100e3fe9c4
정세현 <hyeon@MacBook-Air-3.local>
2025-10-04 22:31:39 +0900

    tiny_dlearn

--- dlearn_project/tiny_dlearn.py
+++ dlearn_project/tiny_dlearn.py
@@ -11,11 +11,11 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 train_dataset = datasets.MNIST(
-    root="./data",                       # 저장위치
-    train=True,                          # 학습용 데이터
-    transform=transforms.ToTensor(),      # 형태: 텐서
+    root="./data",                      # 저장위치
+    train=True,                         # 학습용 데이터
+    transform=transforms.ToTensor(),    # 형태: 텐서
     download=True                       # 없으면 자동 다운로드
-    )                      
+    )                                   
 test_dataset = datasets.MNIST(
     root="./data",
     train=False,
@@ -41,7 +41,7 @@ def show_tensor_images(dataset, n_rows=5, n_cols=10):
     plt.tight_layout()  
     plt.show()
 
-show_tensor_images(train_dataset)
+# show_tensor_images(train_dataset)
 
 
 ## 화이트박스 딥러닝 모델
@@ -82,7 +82,7 @@ class Linear(Layer):
             self.db (ndarray (out_dim, ))
         """
         self.dW = self.x.T @ grad_output
-        self.db = np.sum(grad_output, axis=0, keepdim=True)
+        self.db = np.sum(grad_output, axis=0, keepdims=True)
         dx = grad_output @ self.W.T
         return dx
 
@@ -221,7 +221,7 @@ class Sequential:
             for layer in self.layers:
                 x = layer.forward(x)
             exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-            probs = exp_x / np,sum(exp_x, axis=1, keepdims=True)
+            probs = exp_x / np.sum(exp_x, axis=1, keepdims=True)
             preds.append(np.argmax(probs, axis=1))
         
         return np.concatenate(preds, axis=0)
@@ -230,3 +230,147 @@ class Sequential:
         y_true = np.argmax(y, axis=1)       # 정답 레이블이 원-핫 인코딩되어 있으므로, 1의 위치 찾는 용도, axis는 1
         acc = np.mean(y_pred == y_true) * 100
         return acc
+    def predict_proba(self, X, batch_size=128):
+        probs = []
+        for i in range(0, X.shape[0], batch_size):
+            x = X[i : i + batch_size]
+            for layer in self.layers:
+                x = layer.forward(x)
+            exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+            prob = exp_x / np.sum(exp_x, axis=1, keepdims=True)
+            probs.append(prob)
+        return np.vstack(probs)
+    def train_test_split(X, y, test_size=0.2, shuffle=True, seed=None):
+        n_samples = X.shape[0]
+        if shuffle:
+            if seed is not None:
+                np.random.seed(seed)
+            indices = np.random.permutation(n_samples)
+        else:
+            indices = np.arange(n_samples)
+        test_size = int(n_samples * test_size)
+        test_idx = indices[ : test_size]
+        train_idx = indices[test_size : ]
+        return X[train_idx], X[test_idx], y[train_idx], y[test_idx]
+
+# Scheduler
+
+class LRScheduler:
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
+        self.base_lr = optimizer.lr
+    def step(self, epoch):          # 자식 코드에 step이 반드시 오버라이딩 되어야한다.
+        raise NotImplementedError
+class StepLR(LRScheduler):
+    def __init__(self, optimizer, step_size=10, gamma=0.1):
+        super().__init__(optimizer)
+        self.step_size = step_size
+        self.gamma = gamma
+    def step(self, epoch):
+        if epoch % self.step_size == 0 and epoch > 0:
+            self.optimizer.lr *= self.gamma
+
+# 데이터 전처리
+sample_num = len(train_dataset)
+class_num = 10
+X = []
+y = []
+for idx in range(sample_num):
+    color, label = train_dataset[idx]
+    color = np.array(color[0])
+    color = color.reshape(-1)
+    X.append(color)
+    y.append(np.eye(class_num)[label])
+    
+
+X = np.array(X)
+y = np.array(y)
+
+# K-Fold Cross Validation
+
+def k_fold_split(n_samples, k=5, shuffle=True, seed=None):
+    indices = np.arange(n_samples)
+    if shuffle:
+        if seed is not None:
+            np.random.seed(seed)
+        np.random.shuffle(indices)
+    fold_size = n_samples // k
+    folds = []
+    for i in range(k):
+        val_start = i * fold_size
+        val_end = (i + 1) * fold_size if i < k - 1 else n_samples
+        val_idx = indices[val_start : val_end]
+        train_idx = np.concatenate([indices[: val_start], indices[val_end :]])
+        folds.append((train_idx, val_idx))
+    return folds
+
+
+## 실행
+
+def main_loop(lr, momentum):
+    """
+    X (N, 784)
+    y (N, 10)
+    """
+    activations = [Sigmoid(), Tanh(), ReLU(), Leaky_ReLU()]
+    titles = ["Sigmoid", "Tanh", "ReLU", "Leaky_ReLU"]
+    folds = k_fold_split(X.shape[0], k=len(activations), shuffle=True, seed=42)
+    fold_models = []
+    all_logs = [] # 전체 fold 로그를 저장할 리스트
+    for fold, (train_idx, val_idx) in enumerate(folds):
+        print(f"Fold {fold+1} : {titles[fold]}")
+        print("Trainsize:", len(train_idx), "Val size:", len(val_idx))
+        
+        X_train, y_train = X[train_idx], y[train_idx]
+        X_val, y_val = X[val_idx], y[val_idx]
+
+        layers = [
+            Linear(784, 128),
+            activations[fold],
+            Linear(128, 10)
+        ]
+        loss_fn = MyLoss()
+        optimizer = SGDMomentum(layers, lr, momentum)
+        scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+        model = Sequential(layers, loss_fn, optimizer)
+
+        model.fit(X_train, y_train, epoch=5, batch_size=32, scheduler=scheduler, X_val=X_val, y_val=y_val)
+
+        fold_models.append(model)
+        df_log = pd.DataFrame(model.log)
+        df_log["Activation"] = titles[fold]
+        df_log["Fold"] = fold + 1
+        df_log["learning_rate"] = lr
+        df_log["momentum"] = momentum
+        all_logs.append(df_log)
+
+        acc = model.evaluate(X_val, y_val)
+        print(f"Fold {fold+1} Accuracy: {acc:.2f}%\n")
+
+    final_log = pd.concat(all_logs, ignore_index=True)
+    csv_path = "all_folds_results.csv"
+    if not os.path.exists(csv_path):
+        final_log.to_csv(csv_path, mode="w", header=True, index=False, encoding="utf-8-sig")
+        print("첫 저장 (header 포함)")
+    else:
+        final_log.to_csv(csv_path, mode="a", header=False, index=False, encoding="utf-8-sig")
+        print("이어쓰기 (header 제외)")
+
+interval = 0.05
+for i in range(5):
+    main_loop(0.04, 0.9 - (i + 1) * interval)
+
+
+test_num = len(test_dataset)
+class_num = 10
+X_test = []
+y_test = []
+for idx in range(test_num):
+    color, label = train_dataset[idx]
+    color = np.array(color[0])
+    color = color.reshape(-1)
+    X_test.append(color)
+    y_test.append(np.eye(class_num)[label])
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
